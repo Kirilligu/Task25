bf1f88db7c78ba30a01e0f9c50c1f648e79dbd4d
Kirilligu <k.degtyarev4@mail.ru>
2025-03-04 21:05:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/task25/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.task25
+
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+
+}
